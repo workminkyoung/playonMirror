9b2e3f6a77b2dc03af33e109de246b4248883ff4
minkyoung <workminkyoung@gmail.com>
2024-09-26 17:23:59 +0900

    [Fix] 인화지 체크 방어코드 추가

--- Assets/Scripts/PhotoPaperCheckModule.cs
+++ Assets/Scripts/PhotoPaperCheckModule.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -9,11 +10,53 @@ public static class PhotoPaperCheckModule
 
     public static int GetRemainPhotoPaper()
     {
-        if (!File.Exists(PATH))
+        int remainNum = 0;
+        try
         {
-            File.WriteAllText(PATH, "400");
+            // 파일이 없다면 생성 후 초기화
+            if (!File.Exists(PATH))
+            {
+                File.WriteAllText(PATH, "400");
+            }
+
+            string fileContent = File.ReadAllText(PATH);
+            // 파일 null check
+            if (string.IsNullOrEmpty(fileContent))
+            {
+                CustomLogger.LogWarning("Photo Paper Remain Nothing, Reset To 0");
+                remainNum = 0;
+                fileContent = remainNum.ToString();
+                File.WriteAllText(PATH, fileContent);
+            }
+
+            if (!int.TryParse(fileContent, out remainNum))
+            {
+                CustomLogger.LogWarning("Photo Paper Remain Nothing, Reset To 0");
+                remainNum = 0;
+                fileContent = remainNum.ToString();
+                File.WriteAllText(PATH, fileContent);
+            }
+
+            CustomLogger.Log($"Photo Remain {remainNum}");
         }
-        return int.Parse(File.ReadAllText(PATH));
+        catch (IOException ioEx)
+        {
+            CustomLogger.LogException(ioEx);
+            remainNum = 0;
+        }
+        catch (UnauthorizedAccessException authEx)
+        {
+            CustomLogger.LogException(authEx);
+            remainNum = 0;
+        }
+        catch (Exception ex)
+        {
+            // 그외모든 예외 에러 케이스
+            CustomLogger.LogException(ex);
+            remainNum = 400;
+        }
+
+        return remainNum;
     }
 
     public static void SetRemainPhotoPaper(int remainNum)
